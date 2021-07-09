e96df09b60dc2a318678e03e7e6dbad77f850502
Munish Kumar <65109994+munishkumar-gh@users.noreply.github.com>
2021-07-09 16:36:40 +0800

    Basic Python on Atom

---
+++ SuSS/ANL252/3_Lecturer/1a_Car_Sales_Qn.py
@@ -0,0 +1,4 @@
+# Car sales program:
+# Create a program in which the user is asked to enter the brand, model and
+# selling price of a car. Print the information in one print() command
+# subsequently using formatting printing.
