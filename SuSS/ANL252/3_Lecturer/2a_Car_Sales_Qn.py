713cf6e939b966b3500541d8a876e54c0d93b908
Munish Kumar <65109994+munishkumar-gh@users.noreply.github.com>
2021-07-11 15:47:27 +0800

    Updates

---
+++ SuSS/ANL252/3_Lecturer/2a_Car_Sales_Qn.py
@@ -0,0 +1,13 @@
+# Car sales program:
+# Construct a tuple of the car brands you know (e.g., Toyota, Kia, BMW,
+# Chrysler, Mercedes Benz, Porsche, Fiat, etc.).
+
+# Extend the Car sales program from Study Unit 1 by listing the car brands
+# stored in the tuples and listed them out on the screen when the user is asked
+# to enter the brand of the car.
+
+
+# Note that the user must enter one of the brands in the tuple, i.e.,
+# any brand not stored in the tuple is considered as invalid input.
+# Furthermore, the exact spelling of the brand in the tuple should also be
+# stored in the variable for the car brand input.
